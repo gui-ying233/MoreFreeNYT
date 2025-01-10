a47e838d022f8c47716eee0d8b671536a9969c9e
gui-ying233 <zhuyufei20050307@gmail.com>
2025-01-10 17:32:09 -0500

    fix: :bug: v1.0.1
    增加更多需排除的网址

--- main.user.js
+++ main.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         纽约时报查看更多
 // @namespace    https://github.com/gui-ying233/MoreFreeNYT
-// @version      1.0.0
+// @version      1.0.1
 // @description  去除纽约时报的订阅提示并显示更多内容（不一定能完全显示）
 // @author       鬼影233
 // @license      MIT
 // @match        https://*.nytimes.com/*
 // @exclude      https://*.nytimes.com/
+// @exclude      https://*.nytimes.com/international/
+// @exclude      https://*.nytimes.com/ca/
+// @exclude      https://*.nytimes.com/es/
+// @exclude      https://*.nytimes.com/section/*
 // @icon         https://www.nytimes.com/favicon.ico
 // @supportURL   https://github.com/gui-ying233/MoreFreeNYT/issues
 // @run-at       document-start
@@ -14,7 +18,12 @@
 
 (() => {
 	"use strict";
-	if (document.location.pathname === "/") return;
+	if (
+		["/", "/international/", "/ca/", "/es/", "/section/us"].includes(
+			document.location.pathname
+		)
+	)
+		return;
 	const originalRemoveChild = Element.prototype.removeChild;
 	Element.prototype.removeChild = function (...args) {
 		if (
